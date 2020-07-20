906bca2c3fed070a8c8c59a7643d3579588aa169
Eric Walendzinski <ericwcode@gmail.com>
2020-07-20 19:22:29 -0400

    Add create and delete todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -21,7 +21,29 @@
     
 // });
 
-
-$("li").click(function(){
+// Check off todos by clicking
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
+});
+
+// Click on X to delete todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+
+});
+
+//listener on input
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //get text from input
+        let todoText = $(this).val();
+        $(this).val("");
+        //create new li and add to ul
+        $("ul").append("<li> <span>X</span> " + todoText + "</li>")
+        
+    }
+    
 });
