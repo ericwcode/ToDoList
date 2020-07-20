44c810496a56bb0b6a284b1f0299ba5a18962e59
Eric Walendzinski <ericwcode@gmail.com>
2020-07-20 18:44:44 -0400

    Add Completion Feature

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,27 @@
 // Check off todos by clicking
 
-$("li").click(function(){
-    //if li is gray
-    if ($(this).css("color") === "rgb(128, 128, 128)"){
-        //turn black
-        console.log("its gray. turn black");
-        $(this).css("color", "green"); 
-    }
+// $("li").click(function(){
+//     //if li is gray
+//     if ($(this).css("color") === "rgb(128, 128, 128)"){
+//         //turn black
+//         $(this).css({
+//             color: "black",
+//             textDecoration: "none"
+//         });
+
+//     }
   
-    //else 
-        //turn gray
-    $(this).css({
-        color: "gray",
-        textDecoration: "line-through"
-        }
-    );
+//     //else 
+//         //turn gray
+//     else {$(this).css({
+//         color: "gray",
+//         textDecoration: "line-through"
+//         }
+//     )};
     
+// });
+
+
+$("li").click(function(){
+    $(this).toggleClass("completed");
 });
